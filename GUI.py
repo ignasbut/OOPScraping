8ac4592f9b90f5c561058ca0a35e43f9877ffee7
tomasisaras <tisaras64@gmail.com>
2024-05-01 20:47:22 +0300

    added notifications

--- GUI.py
+++ GUI.py
@@ -1,4 +1,7 @@
+import time
+import threading
 import kivy
+import asyncio
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.widget import Widget
@@ -7,6 +10,8 @@ from kivy.core.window import Window
 from kivy.uix.label import Label
 from kivy.uix.checkbox import CheckBox
 from kivy.uix.popup import Popup
+from kivy.app import async_runTouchApp
+from car import Query
 import sys
 import os
 
@@ -19,52 +24,52 @@ if sys.platform == 'darwin':  # macOS
 elif sys.platform == 'win32' or sys.platform == 'win64':  # Windows
     from win10toast import ToastNotifier
 
-
 # import scraping
 # import threading
 
 Builder.load_file('noticar.kv')
 
 
+
+
 class MyLayout(Widget):
     def __init__(self, app):
         super().__init__()
         self.app = app
-
     car_values = []
 
     def brand_spinner_clicked(self, value):
-        self.ids.brand_spinner.text=value
+        self.ids.brand_spinner.text = value
 
     def mileage_from_clicked(self, value):
-        self.ids.mileage_from_spinner.text=value
+        self.ids.mileage_from_spinner.text = value
 
     def mileage_to_clicked(self, value):
-        self.ids.mileage_to_spinner.text=value
+        self.ids.mileage_to_spinner.text = value
 
     def transmission_clicked(self, value):
-        self.ids.transmission_spinner.text=value
+        self.ids.transmission_spinner.text = value
 
     def fuel_clicked(self, value):
-        self.ids.fuel_spinner.text=value
+        self.ids.fuel_spinner.text = value
 
     def driven_wheels_clicked(self, value):
-        self.ids.driven_wheels_spinner.text=value
+        self.ids.driven_wheels_spinner.text = value
 
-    car_details=[]
+    car_details = []
+    query_list = []
 
-
-    brand_input=ObjectProperty(None)
+    brand_input = ObjectProperty(None)
     model_input = ObjectProperty(None)
     year_from_input = ObjectProperty(None)
     year_to_input = ObjectProperty(None)
-    mileage_from_input=ObjectProperty(None)
-    mileage_to_input=ObjectProperty(None)
-    transmission_input=ObjectProperty(None)
-    engine_vol_input=ObjectProperty(None)
-    driven_wheels_input=ObjectProperty(None)
-    price_from_input=ObjectProperty(None)
-    price_to_input=ObjectProperty(None)
+    mileage_from_input = ObjectProperty(None)
+    mileage_to_input = ObjectProperty(None)
+    transmission_input = ObjectProperty(None)
+    engine_vol_input = ObjectProperty(None)
+    driven_wheels_input = ObjectProperty(None)
+    price_from_input = ObjectProperty(None)
+    price_to_input = ObjectProperty(None)
 
     def clear_input_fields(self):
         self.ids.brand_spinner.text = ''
@@ -97,16 +102,17 @@ class MyLayout(Widget):
                 error_found = True
                 break
 
-
+        # Check for invalid characters in engine volume (only allow .)
         if any(char in self.ids.engine_vol_input.text for char in invalid_chars if char != "."):
             error_found = True
 
         return error_found
+
     def press(self):
         popup = Popup(title='Invalid input(s)',
                       content=Label(text="You used invalid characters. For engine volume use: ."),
-                      size_hint=(0.6, 0.2), pos_hint= {"x": 0.2, "top": 0.9},
-        )
+                      size_hint=(0.6, 0.2), pos_hint={"x": 0.2, "top": 0.9},
+                      )
         label = popup.content
         label.font_size = 24
 
@@ -115,59 +121,56 @@ class MyLayout(Widget):
             popup.open()
             return
 
+        brand = self.ids.brand_spinner.text
+        model = self.model_input.text
 
-        brand=self.ids.brand_spinner.text
-        model=self.model_input.text
+        year_from = self.year_from_input.text
+        year_to = self.year_to_input.text
 
-        year_from =self.year_from_input.text
-        year_to=self.year_to_input.text
-
-        mileage_from=self.ids.mileage_from_spinner.text
+        mileage_from = self.ids.mileage_from_spinner.text
         mileage_to = self.ids.mileage_to_spinner.text
-        transmission=self.ids.transmission_spinner.text
-        engine_vol=self.engine_vol_input.text
+        transmission = self.ids.transmission_spinner.text
+        engine_vol = self.engine_vol_input.text
 
-        fuel=self.ids.fuel_spinner.text
-        driven_wheels=self.ids.driven_wheels_spinner.text
-
+        fuel = self.ids.fuel_spinner.text
+        driven_wheels = self.ids.driven_wheels_spinner.text
 
         price_from = self.price_from_input.text
         price_to = self.price_to_input.text
 
+        query = Query(brand, model, year_from, year_to, mileage_from, mileage_to, transmission,
+                      engine_vol, fuel, driven_wheels, price_from, price_to)
 
-        search_summary = Label(text=f"{brand} {model} {year_from} - {year_to} {mileage_from} - {mileage_to}km {transmission} {engine_vol}L {fuel} {driven_wheels} {price_from} - {price_to} Eur")
+        search_summary = Label(text=query.query_summary())
 
+        self.query_list.append(query)
 
-        self.ids.selected_car.add_widget(search_summary)
+        self.ids.selected_car.clear_widgets()
+
+        for q in self.query_list:
+            self.ids.selected_car.add_widget(Label(text=q.query_summary()))
 
         self.canvas.ask_update()
 
-        print(f'Brand: {brand}, model: {model}, year from: {year_from}, year to: {year_to}, mileage from: {mileage_from}, mileage to: {mileage_to}, transmission: {transmission}, engine volume: {engine_vol}, fuel type: {fuel}, driven wheels: {driven_wheels}, price from: {price_from}, price to: {price_to}')
+        print(
+            f'Brand: {brand}, model: {model}, year from: {year_from}, year to: {year_to}, mileage from: {mileage_from}, mileage to: {mileage_to}, transmission: {transmission}, engine volume: {engine_vol}, fuel type: {fuel}, driven wheels: {driven_wheels}, price from: {price_from}, price to: {price_to}')
 
         self.clear_input_fields()
 
         self.app.show_notification(self)
 
 
-
-
-
-
-        #scraping.conv_obj(brand, model, year_from, year_to, mileage_from, mileage_to, transmission, engine_vol,
-                          #fuel, driven_wheels, price_from, price_to)
-
-
 class NotiCarApp(App):
     def build(self):
-        Window.clearcolor=(28/255.0, 99/255.0, 158/255.0, 0.75)
-        return MyLayout(self)
+        Window.clearcolor = (28 / 255.0, 99 / 255.0, 158 / 255.0, 0.75)
+        return MyLayout()
 
     def show_notification(self, instance):
-        title = "New listing(s!"
+        title = "New listing(s)"
         message = "There are some updates for your request(s)."
 
         script_path = os.path.abspath('show_notification_with_icon.applescript')
-        icon_path= os.path.abspath('added-64.png')
+        icon_path = os.path.abspath('added-64.png')
         script = f'{script_path} {icon_path} "{title}" "{message}"'
 
         if sys.platform == 'win32' or sys.platform == 'win64':  # Windows
@@ -181,7 +184,20 @@ class NotiCarApp(App):
 
         )
 
-
-
 if __name__ == '__main__':
     NotiCarApp().run()
+
+# Async
+
+#
+# loop = asyncio.get_event_loop()
+# loop.run_until_complete(
+#     NotiCarApp().async_run())
+# loop.close()
+
+# Threading
+# t1 = threading.Thread(target=NotiCarApp().run())
+#
+# t1.start()
+# time.sleep(4)
+# t1.join()
