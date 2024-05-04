48a440a2caf9a6f23f0dcedaec13b8bcdc8f9a83
Ignas <iggy.butkus@gmail.com>
2024-05-04 20:25:52 +0300

    started autoplius

--- autoplius.py
+++ autoplius.py
@@ -1,3 +1,19 @@
-def get_objects(*args):
-    print("Autoplius will be scraped later")
-    return []
+website = 'https://autoplius.lt/paieska/naudoti-automobiliai'
+root = 'https://autoplius.lt/'
+
+xpaths = {
+    "make_box": '//select[@id="make_id_list|]',
+    "model_box": '//select[@id="make_id_sublist_portal"]',
+    "price_from": '',
+    "price_to": '',
+    "year_from": '',
+    "year_to": '',
+    "body_type": '',
+    "fuel_type": '',
+    "adv_search": '',
+    "but_submit": '',
+    "paginator": '',
+    "but_next": '',
+    "list_container": '',
+    "captcha_accept": '',
+}
